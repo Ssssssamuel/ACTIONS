e4aad7ad6e832cc259c4367ca064e1801b30d96a
unknown <samyfedyrob@yahoo.com>
2025-01-18 18:01:49 +0000

    Updated terraform-aws.yml

---
+++ C.module1/output.tf
@@ -0,0 +1,3 @@
+output "Key_pair_id" {
+    value = aws_key_pair.Stack_KP.id
+}
---
+++ C.module1/variable.tf
@@ -0,0 +1,7 @@
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "test_key.pub"
+}
+
+variable "ami" {
+  default = "ami-041ac307104473fa1"
+}
