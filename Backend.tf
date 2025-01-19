772b01a728d474e5e76aa9a279e0c69c4565f61d
unknown <samyfedyrob@yahoo.com>
2025-01-19 08:37:27 +0000

    Updated terraform-aws.yml

--- Backend.tf
+++ Backend.tf
@@ -5,6 +5,7 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "statelock-tf"
     encrypt        = true
+    role_arn       = "arn:aws:iam::222634373909:role/Engineer"
     # kms_key_id     = "alias/aws/s3"
     # acl     = "bucket-owner-full-control"
   }
