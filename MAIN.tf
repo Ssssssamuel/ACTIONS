b58b8447020ff4e5a85d6190fc50e0f1309ad8a4
unknown <samyfedyrob@yahoo.com>
2025-01-30 00:21:59 +0000

    Yet another trial

--- MAIN.tf
+++ MAIN.tf
@@ -1,8 +1,14 @@
+locals {
+  env = var.environment
+  account_to_deploy = var.ACCOUNTS[local.env]
+  account_arn = "arn:aws:iam::${local.account_to_deploy}:role/Engineer"
+}
+
 module "my_entire_stack" {
   #source = "./C.module1"
 
   #For remote source
-  source            = "https://github.com/Ssssssamuel/MY-ACTIONS"
+  source            = "https://github.com/Ssssssamuel/Actions-Direc.git"
 
   #AWS_REGION         = var.AWS_REGION
   PATH_TO_PUBLIC_KEY = var.PATH_TO_PUBLIC_KEY
