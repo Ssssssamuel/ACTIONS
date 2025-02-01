06b2bc4e447b8f3d25a11a58f981093a679a7376
unknown <samyfedyrob@yahoo.com>
2025-02-01 15:13:36 +0000

    Running Pipeline

---
+++ test/selenium_test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get("http://localhost:8080")
+assert "CliXX Retail!" in driver.title  # Modify based on actual app title
+driver.quit()
