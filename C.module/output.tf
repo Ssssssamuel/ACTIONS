106ba5b381fa3884a59cae423b6da1ff19f52f8f
unknown <samyfedyrob@yahoo.com>
2025-02-04 08:55:01 +0000

    Refreshing Pipeline

--- C.module/output.tf
+++
@@ -1,3 +0,0 @@
-# output "Security_Goup_id" {
-#     value = aws_security_group.test_sg.id
-# }
--- C.module1/output.tf
+++
@@ -1,7 +0,0 @@
-output "Key_pair_id" {
-    value = aws_key_pair.Stack_KP.id
-}
-
-output "security_group_debug" {
-  value = var.security_groups
-}
